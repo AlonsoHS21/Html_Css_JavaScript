6e1f372021bd5b66073b255c73095215f2368049
AlonsoHS21 <alonsohua20@gmail.com>
2020-12-23 19:57:39 -0300

    07 - Ejercicios Bonus ES6
    Template Linearls

---
+++ plataforma_5/clase_7/template_literals/script.js
@@ -0,0 +1,10 @@
+// Ejercicio 1
+let nombre = "Gabriela"
+let profesion = "programadora"
+console.log(`Soy ${nombre} y trabajo de ${profesion}`)
+
+// Ejercicio 2
+let precio,cantidad
+precio = prompt("Ingrese el precio del articulo")
+cantidad = prompt("Ingrese la cantidad")
+alert(`Precio total es de: ${precio * cantidad}`)
