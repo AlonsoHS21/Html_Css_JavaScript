ae442124789dcba78848c9c1640f1d545e8dd971
AlonsoHS21 <alonsohua20@gmail.com>
2020-12-25 00:58:48 -0300

    08 - Comparando

--- plataforma_5/clase_8/script.js
+++ plataforma_5/clase_8/script.js
@@ -1,4 +1,69 @@
+//Practica JS
+/*
 let edad = prompt("Cual es tu edad?") 
 
 if (edad < 18){alert("Sos menor de edad")}
 else {alert("Sos mayor de edad")}
+*/
+
+//¿Que devuelve cada expresion?
+/*
+var x = 10
+var y = "a"
+y ==="b" || x >= 10 // true --> porque x es igual a 10 con que una sea true basta
+
+var x = 3
+var y = 8
+!(x == "3" || x === y) && !(y !== 8 && x <= y) // false --> porque la primera condicion es true y con el ! (negado) da falso las dos deben ser true
+
+var str = ""
+var msj = "jaja!"
+var esGracioso = "false"
+!((str || msj) && esGracioso) //false --> el negado vuelve falso todo
+*/
+
+//Comparando
+// 1 -
+/*
+let string1 = "hola1"
+let string2 = "hola1"
+if(string1 == string2){
+    alert("Es true")
+}
+else {
+    alert("Es false")
+}
+*/
+// 2 - 
+/*
+let string1 = "hola1"
+let string2 = "hola2"
+if(string1 == string2){
+    alert("Es true")
+}
+else {
+    alert("Es false")
+}
+*/
+// 3 -
+/*
+let string = "1"
+let number = 1
+if(string == number){
+    alert("Es true")
+}
+else {
+    alert("Es false")
+}
+*/
+// 4 - 
+/*
+let string = "2"
+let number = 1
+if(string == number){
+    alert("Es true")
+}
+else {
+    alert("Es false")
+}
+*/
